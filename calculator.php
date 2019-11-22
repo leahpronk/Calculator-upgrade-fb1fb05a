ac0fb9cca0ca22d1fb3b385548ececa4905970cd
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2019-11-22 11:46:19 +0100

    Uitwerking van de exercise

--- calculator.php
+++ calculator.php
@@ -1,6 +1,19 @@
 <?php
 
 $calculator = readline ("welke uitvoering wil je? (+,-,%)");
+$antwoord1 = readline ("wat is je eerste cijfer?");
+
+if (is_numeric($antwoord1)){
+	echo "";
+} else {
+	exit("Dit is geen getal");
+}
+$antwoord2 = readline ("wat is je tweede cijfer?");
+if (is_numeric($antwoord2)){
+	echo "";
+} else {
+	exit("Dit is geen getal");
+}
 
 switch ($calculator) {
 	case '+':
@@ -17,19 +30,6 @@ switch ($calculator) {
 	default:
 		exit("ERROR");
 		break;
-}
-
-$antwoord1 = readline ("wat is je eerste cijfer?");
-if (is_numeric($antwoord1)){
-	echo "";
-} else {
-	exit("Dit is geen getal");
-}
-$antwoord2 = readline ("wat is je tweede cijfer?");
-if (is_numeric($antwoord2)){
-	echo "";
-} else {
-	exit("ERROR");
 }
 
 
